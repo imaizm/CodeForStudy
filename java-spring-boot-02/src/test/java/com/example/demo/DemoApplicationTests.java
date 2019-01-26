1608cc602c0accc410719e4866a3d183c1328f34
imaizm <imaizm@gmail.com>
2019-01-27 03:15:27 +0900

    テストコード追加

--- java-spring-boot-02/src/test/java/com/example/demo/DemoApplicationTests.java
+++ java-spring-boot-02/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,16 +2,41 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().string("Hello world"));
+	}
+	
+	@Test
+	public void getUsers() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/users").accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andDo(MockMvcResultHandlers.print());
+		
+	}
 }
 
