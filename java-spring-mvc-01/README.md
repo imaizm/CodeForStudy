17db6f2af4f4fde5447e4464cbd6499d4000ccab
imaizm <imaizm@gmail.com>
2019-01-27 22:18:15 +0900

    java-spring-mvc-01追加

---
+++ java-spring-mvc-01/README.md
@@ -0,0 +1,5 @@
+# SpringBoot習作サンプル
+
+[このページ][1]
+の内容を試しに実装してみたもの    
+[1]:http://seiritozakki.hatenablog.com/entry/spring-mvc-01
---
+++ java-spring-mvc-01/src/main/java/com/example/controller/user/UserController.java
@@ -0,0 +1,20 @@
+package com.example.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public String goToRegister() {
+		return "user/userRegister";
+	}
+	
+	@RequestMapping(value="/reissue", method=RequestMethod.GET)
+	public String goToReissue() {
+		return "user/userReissue";
+	}
+}
